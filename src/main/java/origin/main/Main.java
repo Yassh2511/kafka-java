9873fca11166508f71b31ef4a02efe43673ff2c0
yash <yash.s.war63@gmail.com>
2025-09-11 00:07:56 +0530

    Broker can accepts correlatio_id from client

--- src/main/java/origin/main/Main.java
+++ src/main/java/origin/main/Main.java
@@ -18,6 +18,11 @@ public class Main {
         serverSocket.setReuseAddress(true);
         // Wait for connection from client.
         clientSocket = serverSocket.accept();
+        // sending correlation_id to the client
+        // correlation id is something which is 32 bit
+        // here sending direct correlation id only which is 7
+        // so first 0000 is for message size =0 and next 0007 this 4 bytes is for correlation id
+        clientSocket.getOutputStream().write(new byte[]{0,0,0,0,0,0,0,7});
     } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
     } finally {
